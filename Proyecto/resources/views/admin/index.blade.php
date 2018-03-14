893b991f9c65451a31c56e8e2b0c344646963d42
juan <jmbt5@alu.ua.es>
2018-03-14 01:59:09 +0100

    completando perfil admi

--- Proyecto/resources/views/admin/index.blade.php
+++ Proyecto/resources/views/admin/index.blade.php
@@ -1,49 +1,40 @@
 @extends('layout')
 
 @section('content')
+
 <div class="card mb-3">
-<div class="card-header">
-  <i class="fa fa-table"></i> Relación de Empleados</div>
-<div class="card-body">
-  <div class="table-responsive">
-    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-      <thead>
-        <tr>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Fecha de Nacimiento</th>
-            <th>Contrato</th>
-            <th>Teléfono</th>
-            <th>Ver</th>
-            <th>Modificar</th>
-            <th>Eliminar</th>
+  <div class="card-header"><i class="fa fa-table"></i> Data Table Example</div>
+  <div class="card-body">
+    <div class="table-responsive">
+      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+        <thead>
+          <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th></th>
+              
+          </tr>
+          </thead>
 
-        </tr>
-        </thead>
-
-        <tbody>
-        @foreach($employees as $employee)
-        <tr>
-            <td>{{ $employee->id }}</td>
-            <td>{{ $employee->name }}</td>
-            <td>{{ $employee->email }}</td>
-            <td>{{ $employee->birthdate }}</td>
-            <td>{{ $employee->contract }}</td>
-            <td>{{ $employee->phone }}</td>
-            <td><a class="btn btn-default" href="#" role="button">Link</a></td>
-            <td>modificar</td>
-            <td>eliminar</td>
-        </tr>
-        @endforeach
-        
+          <tbody>
+          @foreach($employees as $employee)
+          <tr>
+              <td>{{ $employee->id }}</td>
+              <td>{{ $employee->name }}</td>
+              <td>{{ $employee->email }}</td>
+              <td><a type="button" class="btn btn-success btn-sm" href="{{ route('admin.show', $employee->id)}}" role="button">Ver     </a>
+              <a type="button" class="btn btn-success btn-sm" href="{{ route('admin.show', $employee->id)}}" role="button">Modificar</a>
+              <a type="button" class="btn btn-success btn-sm" href="{{ route('admin.show', $employee->id)}}" role="button">Eliminar</a></td>
+          </tr>
+          @endforeach
+          
+          </tbody>
+        </table>
         </tbody>
-    </table>
-    </tbody>
-    </table>
+      </table>
+    </div>
   </div>
-</div>
-<div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
 </div>
 
 @endsection
--- Proyecto/resources/views/admin/show.blade.php
+++ Proyecto/resources/views/admin/show.blade.php
@@ -1,45 +1,55 @@
 @extends('layout')
 
 @section('content')
-<div class="card mb-3">
-<div class="card-header">
-  <i class="fa fa-table"></i> Relación de Empleados</div>
-<div class="card-body">
-  <div class="table-responsive">
-    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-      <thead>
-        <tr>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Fecha de Nacimiento</th>
-            <th>Contrato</th>
-            <th>Teléfono</th>
-            <th>Modificar</th>
-            <th>Eliminar</th>
-
-        </tr>
-        </thead>
-
-        <tbody>
-        <tr>
-            <td>{{ $employee->id }}</td>
-            <td>{{ $employee->name }}</td>
-            <td>{{ $employee->email }}</td>
-            <td>{{ $employee->birthdate }}</td>
-            <td>{{ $employee->contract }}</td>
-            <td>{{ $employee->phone }}</td>
-            <td>modificar</td>
-            <td>eliminar</td>
-        </tr>
-        
-        </tbody>
-    </table>
-    </tbody>
-    </table>
+<div class="row">
+<div class="col-lg-4">
+  <div class="card mb-3">
+    <div class="card-header"><i class="fa fa-bar-chart"></i> Bar Chart Example</div>
+    <div class="card-body">
+      <div class="text-center"> <img src="../images/e1.jpeg"class="img-fluid" alt="..."> </div>
+      <canvas id="myBarChart" width="100" height="50"></canvas>
+    </div>
   </div>
 </div>
-<div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+
+
+    <div class="col-lg-8">
+      <!-- Example Bar Chart Card-->
+      <div class="card mb-3">
+        <div class="card-header">
+          <i class="fa fa-bar-chart"></i> Bar Chart Example</div>
+        <div class="card-body">
+        <form>
+              <div class="form-group">
+                <label for="inputAddress">Nombre</label>
+                <input type="text" class="form-control" id="inputAddress" value="{{ $employee->name }}">
+              </div>
+              <div class="form-group">
+                <label for="inputAddress2">Email</label>
+                <input type="text" class="form-control" id="inputAddress2" value="{{ $employee->email }}">
+              </div>
+              <div class="form-group">
+                <label for="inputAddress2">Fecha de Nacimiento</label>
+                <input type="text" class="form-control" id="inputAddress2" value="{{ $employee->birthdate }}">
+              </div>
+              <div class="form-group">
+                <label for="inputAddress2">Contrato</label>
+                <input type="text" class="form-control" id="inputAddress2" value="{{ $employee->contract }}">
+              </div>
+              <div class="form-group">
+                <label for="inputAddress2">Telefono</label>
+                <input type="text" class="form-control" id="inputAddress2" value="{{ $employee->phone }}">
+              </div>
+          
+              <button type="submit" class="btn btn-primary">Sign in</button>
+            </form>
+          <canvas id="myBarChart" width="100" height="50"></canvas>
+        </div>
+      </div>
+    </div>
+    
+  
 </div>
+
 
 @endsection
