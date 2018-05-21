0ebf7d893f1eaf08150b8062756873d4c45af430
Jose Carlos Jurado <jcjg3@alu.ua.es>
2018-05-21 09:26:00 +0200

    Subiendo arreglos de la base de datos

--- Proyecto/database/migrations/2018_05_21_072355_create_expedients_table.php
+++ Proyecto/database/migrations/2018_05_21_072355_create_expedients_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRecordsTable extends Migration
+class CreateExpedientsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateRecordsTable extends Migration
      */
     public function up()
     {
-        Schema::create('records', function (Blueprint $table) {
+        Schema::create('expedients', function (Blueprint $table) {
             $table->increments('id');
             $table->string('diagnosis');
-            $table->String('enclosed');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ class CreateRecordsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('records');
+        Schema::dropIfExists('expedients');
     }
 }
--- Proyecto/init.sh
+++ Proyecto/init.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash
 composer install
+composer require maddhatter/laravel-fullcalendar
 cp .env.example .env
 touch database/database.sqlite
 php artisan migrate:install
